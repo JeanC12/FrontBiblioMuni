9e511dfc06f6d02ead716005de853c3894df4656
jeanHW <jeancarloshw2020@gmail.com>
2024-11-25 21:34:48 -0500

    env atuth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    endpoint: 'http://localhost:3000/',
+    endpoint: 'https://book-service-6t5i.onrender.com/',
     isbn: 'https://databookpy.onrender.com/',
     portrait: 'https://portraitbook.onrender.com/'
 };
